472d75e3470da4c874ca48942a0f3e63f187003e
AndresSal <andres.salazar01@epn.edu.ec>
2017-02-03 21:59:08 -0500

    metodo validar ErrorProvider

--- Libreria/Class1.cs
+++ Libreria/Class1.cs
@@ -43,7 +43,7 @@ namespace Libreria
                         }else
                         {
                             ErrorProvider.SetError(obj,"");
-
+                        
                         }
                     }
                 }
