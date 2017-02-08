5848dfce82fbb3aad1166a1532610a854db1591a
Danni <daniebro.5@gmail.com>
2017-02-08 17:00:14 -0500

    CambiosReservaciones

--- JUDYSOFT/FormReservas.Designer.cs
+++ JUDYSOFT/FormReservas.Designer.cs
@@ -32,18 +32,18 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tablaReservas = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaReservas)).BeginInit();
             this.SuspendLayout();
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(161, 241);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Location = new System.Drawing.Point(215, 297);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(102, 24);
+            this.button5.Size = new System.Drawing.Size(136, 30);
             this.button5.TabIndex = 13;
             this.button5.Text = "Cancelar Reserva";
             this.button5.UseVisualStyleBackColor = true;
@@ -51,10 +51,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(311, 241);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(415, 297);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 24);
+            this.button4.Size = new System.Drawing.Size(125, 30);
             this.button4.TabIndex = 12;
             this.button4.Text = "Nueva Reserva";
             this.button4.UseVisualStyleBackColor = true;
@@ -62,10 +62,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(377, 27);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(503, 33);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(167, 27);
+            this.button3.Size = new System.Drawing.Size(223, 33);
             this.button3.TabIndex = 11;
             this.button3.Text = "Consultar Reservas Pasadas";
             this.button3.UseVisualStyleBackColor = true;
@@ -73,41 +73,40 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(24, 241);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(32, 297);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 24);
+            this.button2.Size = new System.Drawing.Size(123, 30);
             this.button2.TabIndex = 10;
             this.button2.Text = "Consultar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // dataGridView1
+            // tablaReservas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 60);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(522, 162);
-            this.dataGridView1.TabIndex = 9;
+            this.tablaReservas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaReservas.Location = new System.Drawing.Point(29, 74);
+            this.tablaReservas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tablaReservas.Name = "tablaReservas";
+            this.tablaReservas.RowTemplate.Height = 24;
+            this.tablaReservas.Size = new System.Drawing.Size(696, 199);
+            this.tablaReservas.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 40);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(29, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.Size = new System.Drawing.Size(120, 17);
             this.label1.TabIndex = 8;
             this.label1.Text = "Futuras Reservas";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(441, 241);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(588, 297);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 24);
+            this.button1.Size = new System.Drawing.Size(137, 30);
             this.button1.TabIndex = 7;
             this.button1.Text = "Atrás";
             this.button1.UseVisualStyleBackColor = true;
@@ -115,21 +114,22 @@
             // 
             // FormReservas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 293);
+            this.ClientSize = new System.Drawing.Size(749, 361);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tablaReservas);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormReservas";
             this.Text = "FormReservas1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormReservas_FormClosed);
             this.Load += new System.EventHandler(this.FormReservas_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaReservas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,7 +141,7 @@
         private System.Windows.Forms.Button button4;
         public System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView tablaReservas;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
     }
